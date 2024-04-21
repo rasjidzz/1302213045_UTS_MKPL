e39c03f15f00c9f287df967c02fc2bf42e27bfc8
rasjidzz <mrisjads@gmail.com>
2024-04-21 22:06:17 +0700

    Implementasi Switch Statements

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -46,23 +46,23 @@ public class Employee implements TaxCalculable{
 		childIdNumbers = new LinkedList<String>();
 	}
 	@Override
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	public void setMonthlySalary(int grade) {
+		switch (grade) {
+			case 1:
+				monthlySalary = calculateBaseSalary(3000000);
+				break;
+			case 2:
+				monthlySalary = calculateBaseSalary(5000000);
+				break;
+			case 3:
+				monthlySalary = calculateBaseSalary(7000000);
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid grade: " + grade);
 		}
+	}
+	private int calculateBaseSalary(int baseSalary) {
+		return isForeigner ? (int) (baseSalary * 1.5) : baseSalary;
 	}
 	@Override
 	public void setAnnualDeductible(int deductible) {	
